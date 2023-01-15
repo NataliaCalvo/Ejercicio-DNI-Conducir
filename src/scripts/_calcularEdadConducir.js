e0e4eadb49460b1a41c184c4939f688ab013388e
Natalia Calvo Torel <116891081+NataliaCalvo@users.noreply.github.com>
2023-01-15 21:54:54 +0100

    Add files via upload

---
+++ src/scripts/_calcularEdadConducir.js
@@ -0,0 +1,10 @@
+var edad=prompt("Introduzca su edad");
+
+    if(Number(edad) == edad){
+        if(edad>18){
+            document.write("Puedes conducir");
+            }
+    }
+    else {
+        alert("Introduce un número correcto");
+    }
